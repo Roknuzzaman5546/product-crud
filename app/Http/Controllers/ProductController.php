d8e0cb7d15b624094195d927e423f84d4963a52a
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2024-12-08 19:13:38 +0600

    product code added

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,13 +4,21 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+
+    public function index()
+    {
+        $products = Product::all();
+        return view('index', compact('products'));
+    }
+
+
     public function store(Request $request)
     {
         $validated = $request->validate([
             'name' => 'required|string|max:255',
             'price' => 'required|numeric|min:0',
             'quantity' => 'required|integer|min:0',
-        ]);    
+        ]);
         Product::create($validated);
         return redirect()->route('products.index')->with('success', 'Product created successfully!');
     }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,3 +1,5 @@
+<?php
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -6,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
-
     protected $fillable = ['name', 'description', 'price', 'quantity'];
 }
+
